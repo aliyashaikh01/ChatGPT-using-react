5b42bc199a70d0faa742cdb7df824c4e55c0e3a6
aliyashaikh01 <aliyashaikhh0104@gmail.com>
2025-08-01 17:39:35 +0530

    fourth commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useContext } from "react";
 import ChatSection from "./components/ChatSection/ChatSection";
 import Sidebar from "./components/Sidebar/Sidebar";
-import Separation from "./components/separation/separation";
+import Separation from "./components/Separation/Separation";
 import { dataContext } from "./Context/UserContext";
 
 function App() {
